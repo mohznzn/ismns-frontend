42f40b9350e960b03a6b099bab32204b20d09aa6
mohznzn <mohcineharmony@gmail.com>
2025-07-19 22:54:26 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -15,7 +15,7 @@ export default function Home() {
 
 
     try {
-      const res = await fetch("http://localhost:8000/generate_qcm", {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/generate_qcm`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
