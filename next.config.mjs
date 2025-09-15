bca6eeefa3c8f106cab733b9d59220a28c5e2008
ZIZI Mohcine <mohcine.zizi@cegedim.com>
2025-09-15 21:53:58 +0200

    maj

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async redirects() {
+    return [
+      // /admin/myqcms  -> /admin/qcm
+      { source: "/admin/myqcms", destination: "/admin/qcm", permanent: true },
+      // et tout sous-chemin éventuel
+      { source: "/admin/myqcms/:path*", destination: "/admin/qcm/:path*", permanent: true },
+    ];
+  },
+};
 
 export default nextConfig;
+
